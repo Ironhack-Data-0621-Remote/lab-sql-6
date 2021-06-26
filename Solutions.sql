88b64119fb450834e75e204e6c7f987cf5154d29
Yuri Awano <awano.yuri@gmail.com>
2021-06-26 23:05:54 +0200

    lab-sql-6 Yuri Awano

--- Solutions.sql
+++ Solutions.sql
@@ -3,7 +3,7 @@
 -- Now we have received the film catalog for 2020 as well. 
 -- For this new data we will create another table and bulk insert all the data there. 
 -- Code to create a new table has been provided below.
-
+use sakila;
 drop table if exists films_2020;
 CREATE TABLE `films_2020` (
   `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
@@ -30,11 +30,34 @@ CREATE TABLE `films_2020` (
 
 
 -- 1. Add the new films to the database.
+
+-- below didnt work so I tried with import wizard
+-- show variables like 'local_infile';
+-- SET global local_infile = 1;
+
+-- LOAD DATA LOCAL INFILE '/Users/yuriawano/lab-sql-6/films_2020.csv'
+-- INTO table films_2020
+-- FIELDS TERMINATED BY ',';
+
+
+use sakila;
+SELECT * FROM films_2020;
+
+
 -- 2. Update information on `rental_duration`, `rental_rate`, and `replacement_cost`.
 	-- ### Hint
 	-- You might have to use the following commands to set bulk import option to `ON`:
 
-		show variables like 'local_infile';
-		set global local_infile = 1;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE films_2020
+SET rental_duration = '3 days', rental_rate = '2.99€', replacement_cost = '8.99€';
+
+SELECT * FROM films_2020;
+
+
+
+		-- show variables like 'local_infile';
+		-- set global local_infile = 1;
 
 -- If bulk import gives an unexpected error, you can also use the `data_import_wizard` to insert data into the new table.
